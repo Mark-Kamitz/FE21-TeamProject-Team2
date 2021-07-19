decefe40f7786b7122dc0c30cbdece24abc281e3
Marko Tomic <marko.93.tomic@gmail.com>
2021-07-19 23:01:55 +0200

    added products

---
+++ Zumm-Gmbh/src/app/portfolio.ts
@@ -0,0 +1,130 @@
+export const projects = [
+  {
+    image: 'https://cdn.pixabay.com/photo/2017/05/11/08/48/woman-2303361_960_720.jpg',
+    id: 'portraits',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2018/01/13/19/39/fashion-3080644_960_720.jpg',
+    id: 'portraits',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2018/03/06/22/57/portrait-3204843_960_720.jpg',
+    id: 'portraits',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2015/11/26/00/14/woman-1063100_960_720.jpg',
+    id: 'portraits',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2016/03/23/04/01/woman-1274056_960_720.jpg',
+    id: 'portraits',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_960_720.jpg',
+    id: 'nature',
+  },
+
+  {
+    image: 'https://cdn.pixabay.com/photo/2015/07/05/10/18/tree-832079_960_720.jpg',
+    id: 'nature',
+  },
+
+  {
+    image: 'https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_960_720.jpg',
+    id: 'nature',
+  },
+
+  {
+    image: 'https://cdn.pixabay.com/photo/2017/12/29/18/47/mountains-3048299_960_720.jpg',
+    id: 'nature',
+  },
+
+  {
+    image: 'https://cdn.pixabay.com/photo/2014/12/04/14/46/magnolia-trees-556718_960_720.jpg',
+    id: 'nature',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2017/06/07/10/47/elephant-2380009_960_720.jpg',
+    id: 'animals',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2015/04/10/01/41/fox-715588_960_720.jpg',
+    id: 'animals',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2017/01/14/12/59/iceland-1979445_960_720.jpg',
+    id: 'animals',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg',
+    id: 'animals',
+  },
+
+  {
+    image: 'https://cdn.pixabay.com/photo/2016/12/13/00/13/rabbit-1903016_960_720.jpg',
+    id: 'animals',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2014/01/18/10/14/vaulted-cellar-247391_960_720.jpg',
+    id: 'architecture',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041_960_720.jpg',
+    id: 'architecture',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2015/01/28/23/10/mosque-615415_960_720.jpg',
+    id: 'architecture',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/23/18/41/light-5083606_960_720.jpg',
+    id: 'architecture',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2018/06/04/16/00/alley-3453557_960_720.jpg',
+    id: 'architecture',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2014/12/09/21/05/still-life-562370_960_720.jpg',
+    id: 'fine art',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2018/05/16/20/06/still-life-3406891_960_720.jpg',
+    id: 'fine art',
+  },
+
+  {
+    image: 'https://cdn.pixabay.com/photo/2019/06/24/07/56/surreal-4295436_960_720.jpg',
+    id: 'fine art',
+  },
+
+  {
+    image: 'https://cdn.pixabay.com/photo/2018/08/08/17/57/tulips-3592791_960_720.jpg',
+    id: 'fine art',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2019/10/08/04/22/blue-4534094_960_720.jpg',
+    id: 'fine art',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg',
+    id: 'cars',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2016/02/13/13/11/oldtimer-1197800_960_720.jpg',
+    id: 'cars',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2016/09/11/16/47/car-1661767_960_720.jpg',
+    id: 'cars',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2016/11/29/10/01/vw-bulli-1868890_960_720.jpg',
+    id: 'cars',
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2018/02/21/03/15/bmw-m4-3169357_960_720.jpg',
+    id: 'cars',
+  }
+
+];
--- Zumm-Gmbh/src/app/portfolio/portfolio.component.ts
+++ Zumm-Gmbh/src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { projects } from '../portfolio';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
-
+products = projects
   constructor() { }
 
   ngOnInit(): void {
