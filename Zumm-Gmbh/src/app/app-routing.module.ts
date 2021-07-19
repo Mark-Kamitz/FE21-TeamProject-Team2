c7a3950c6f75b80690b26badc15f1c509cc374b8
Marko Tomic <marko.93.tomic@gmail.com>
2021-07-19 15:27:18 +0200

    starting

--- Zumm-Gmbh/src/app/app-routing.module.ts
+++ Zumm-Gmbh/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CustomersComponent } from './customers/customers.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path: "home", component: HomeComponent
+},
+{
+path: "about", component: AboutComponent
+},
+{
+path: "contact", component: ContactComponent
+},
+{
+path: "customers", component: CustomersComponent
+},
+{
+path: "portfolio", component: PortfolioComponent
+},
+{
+path: "testimonials", component: TestimonialsComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
