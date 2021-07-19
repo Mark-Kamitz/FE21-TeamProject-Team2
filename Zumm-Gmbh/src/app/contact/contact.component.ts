954422092f06c5e6ee0262e5edcb31da9e7f704f
Marko Tomic <marko.93.tomic@gmail.com>
2021-07-19 20:09:47 +0200

    navbar and contact page completed

--- Zumm-Gmbh/src/app/contact/contact.component.ts
+++ Zumm-Gmbh/src/app/contact/contact.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
 }
+
+
